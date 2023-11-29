6278fe305e3b6db083caa7697c68b2d95abde9cf
Volodymyr Kysil <vavva67@gmail.com>
2023-11-29 21:42:08 +0100

    added js

---
+++ app.js
@@ -0,0 +1,11 @@
+let cart2El = document.querySelector(".cart-2");
+let addToCartBtns = document.querySelector(".add-to-cart");
+
+console.log(cart2El);
+console.log(addToCartBtns);
+
+// for (let i = 0; i < addToCartBtns.length; i++) {
+//   addToCartBtns[i].addEvenListener("click", function () {
+//     console.log("clicked");
+//   });
+// }
